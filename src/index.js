92d03574267a541fe4539b4ec0b986122bd648bc
Vasily93 <markovavasily@gmail.com>
2022-09-25 14:05:09 -0700

    added correct info to paintings

--- src/index.js
+++ src/index.js
@@ -18,48 +18,19 @@ import img4BW from './images/4BW.JPG'
 import img5BW from './images/5BW.JPG'
 import img6BW from './images/6BW.JPG'
 import img7BW from './images/7BW.JPG'
-// import img8BW from './images/8BW.JPG'
-
-const texts = [
-  `
-The idea for the picture was born after watching a video where Russian mothers do not believe their sons
-(soldiers) who were capturedin Ukraine. Propaganda turned out to be so effective that even the primordial 
-maternal instinct began to fail in women. This is a grotesque reference 
-to the popular phrase attributed to Marshal Zhukov 
-“Women will give birth to new ones”. 
-It expresses the contemptuous attitude of the authorities and generals, 
-both to mothers and women in general, and their soldiers.
-`,
-`
-A woman covers her son or husband
-as a child with a veil of Earth.
-She sings her last lullaby.
-The artist lived in Crimea for a long time.
-The landscape was inspired by the
-theme of a quiet and dark Ukrainian
-night in Crimea.
-`,
-`
-The painting was created under the
-impression of the massacres in Bucha
-and other regions of Ukraine.
-The victims of the tragedy in Bucha
-were shot with hands tied behind their
-backs and bags on their heads.
-`
-]
+import texts from './texts'
 
 const images = [
   //left side
   {position: [-4.5, 0, -1.5], rotation: [0, 0.6, 0], url: [img1, img1bw] , name: 'DANCE OF BIRTH AND DEATH', description: texts[0]},
-  {position: [-4, 0, 1.5], rotation: [0, 1.5, 0], url: [img7, img7BW] , name: 'DANCE OF BIRTH AND DEATH', description: 'No description yet'},
+  {position: [-4, 0, 1.5], rotation: [0, 1.5, 0], url: [img7, img7BW] , name: 'ROLLING UP THE SCROLL OF LIFE', description: texts[5]},
   //middle
-  {position: [0, 0, -2.5], rotation: [0, 0, 0], url: [img6, img6BW] , name: 'ANGEL', description: 'No Description yet'},
+  {position: [0, 0, -2.5], rotation: [0, 0, 0], url: [img6, img6BW] , name: 'EARTH AND SKY', description: texts[3]},
   //outer middle
-  {position: [1.7, 0, 0], rotation: [0, -0.7, 0], url: [img2, img2BW] , name: 'BUCHA', description: texts[2]},
-  {position: [-1.7, 0, 0], rotation: [0, 0.7, 0], url: [img4, img4BW] , name: 'IMAGE 4', description: 'No description yet'},
+  {position: [1.7, 0, 0], rotation: [0, -0.7, 0], url: [img2, img2BW] , name: 'SIGHTLESS', description: texts[2]},
+  {position: [-1.7, 0, 0], rotation: [0, 0.7, 0], url: [img4, img4BW] , name: 'DISINCARNATION', description: texts[6]},
   //right
-  {position: [4.5, 0, -1.5], rotation: [0, -0.6, 0], url: [img3, img3BW] , name: 'IMAGE 3', description: 'No description yet'},
+  {position: [4.5, 0, -1.5], rotation: [0, -0.6, 0], url: [img3, img3BW] , name: 'REFLECTION OF SORROW', description: texts[4]},
   {position: [4, 0, 1.5], rotation: [0, -1.5, 0], url: [img5, img5BW] , name: 'LAST LULLABY', description: texts[1]},
   
 ]
---
+++ src/texts.js
@@ -0,0 +1,51 @@
+
+const texts = [
+  `
+The idea for the picture was born after watching a video where Russian mothers do not believe their sons
+(soldiers) who were capturedin Ukraine. Propaganda turned out to be so effective that even the primordial 
+maternal instinct began to fail in women. This is a grotesque reference 
+to the popular phrase attributed to Marshal Zhukov 
+“Women will give birth to new ones”. 
+It expresses the contemptuous attitude of the authorities and generals, 
+both to mothers and women in general, and their soldiers.
+`,
+`
+A woman covers her son or husband
+as a child with a veil of Earth.
+She sings her last lullaby.
+The artist lived in Crimea for a long time.
+The landscape was inspired by the
+theme of a quiet and dark Ukrainian
+night in Crimea.
+`,
+`
+The painting was created under the
+impression of the massacres in Bucha
+and other regions of Ukraine.
+The victims of the tragedy in Bucha
+were shot with hands tied behind their
+backs and bags on their heads.
+`
+,
+`
+In which we as supplicants look to the sky, and the Lord gazes back. In each of us, there is a fraction of goodness hidden within, only noticed when we see and acknowledge each other.
+`
+,
+`
+Human spirits, snuffed out by circumstance as the wind blows away a candle’s flame. Only the crosses looming in a graveyard remain.
+`
+,
+`
+The angel is rolling up the scroll of human existence. A reflection of the discussion of the possibility of a nuclear holocaust.
+`
+,
+`
+Souls streaming upward, casting off their physical forms—this is the consequence of war.
+`
+,
+`
+Watchers cast away their ability to discern truth from fiction and are consumed by the propagandist’s lies. There is only one truth in war: suffering and death. This work was inspired by conversations with regular people living in Russia.
+`
+]
+
+export default texts;
